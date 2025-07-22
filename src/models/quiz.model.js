859e2256c4376e4933da1252ab15362e08fd79a7
salma457 <you@example.com>
2025-07-22 21:45:03 +0300

    Create quiz model

---
+++ src/models/quiz.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const quizSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    subject: { type: String, required: true },
+    semester: { type: String, required: true },
+    duration: { type: Number, required: true }, // in minutes
+    totalMarks: { type: Number, required: true },
+    questions: {
+      type: [Object], // array of question objects
+      default: [],
+    },
+    authorName: { type: String, required: true },
+    authorAvatar: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Quiz", quizSchema);
